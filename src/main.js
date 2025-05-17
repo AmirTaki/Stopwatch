af619a12841d1ebf5f23aa233346d612136b15b3
amir <amirtaki623@gmail.com>
2025-05-17 20:19:12 +0330

    edit js : time & start & stop , interval

--- src/main.js
+++ src/main.js
@@ -3,27 +3,66 @@ let stopBtn = document.getElementById('stopBtn')
 let startBtn = document.getElementById('startBtn')
 let resetBtn = document.getElementById('resetBtn')
 
-let mesc = 0;
-let secs = 0;
-let mins = 0;
+let hour = '0';
+let mins = '0';
+let secs = '0';
 
 let timerId = null;
 
 const startTimer = ()=> {
-    mesc++
-    if (mesc == 100){
-        msec = 0;
-        secs ++;
-        if(secs == 60){
-            secs = 0;
-            mins ++;  
-        }
+    secs ++;
+    if (secs == 100){
+        secs = 0
+        mins ++ ;
     }
+    if (mins == 60){
+            mins = 0
+            hour ++;
+     }
+
+
+    let secsStr = secs < 10 ? `0${secs}` : secs;
+
+    let minsStr = mins < 10 ? `0${mins}` : mins;
+    
+    let hourStr = hour < 10 ? `0${hour}` : hour;
+
+    console.log(hourStr, minsStr, secsStr )
+
+    timerDisplay.innerHTML = `${hourStr} : ${minsStr} : ${secsStr}`
 }
 
-startBtn.addEventListener('click', (e)=>{
-    if (timerId !== null){
-           clearInterval(timerId)
-    }   
-    timerId  = setInterval(startTimer, 10)
+// strat 
+startBtn.addEventListener("click", (e) => {
+    timerId =  setInterval(startTimer, 10)
 })
+
+// stop 
+stopBtn.addEventListener("click", (e) => {
+   clearInterval(timerId)
+})
+
+
+// start
+// startBtn.addEventListener('click', (e)=>{
+
+//     clearInterval = () =>
+
+//     if (timerId !== null){
+//            clearInterval(timerId)
+//     }   
+//     timerId  = setInterval(startTimer, 10)
+// })
+// // stop
+// stopBtn.addEventListener('click', (e)=>{
+//     clearInterval(timerId)   
+// })
+
+// // reset
+// resetBtn.addEventListener('click', (e)=>{
+//     clearInterval(timerId)  
+//     timerDisplay.innerHTML = "00 : 00 : 00" 
+//     msec = secs = mins = 0
+// })
+
+
